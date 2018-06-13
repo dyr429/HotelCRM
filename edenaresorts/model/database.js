e6308c5400ba47f87e42a74bdccb05914814088e
Yiren Ding <ding.yir@husky.neu.edu>
2018-06-13 14:39:04 -0400

    update database URL

--- edenaresorts/model/database.js
+++ edenaresorts/model/database.js
@@ -5,7 +5,7 @@ if(process.env.MLAB_USERNAME_EDENA) { // check if running remotely
     var username = process.env.MLAB_USERNAME_EDENA; // get from environment
     var password = process.env.MLAB_PASSWORD_EDENA;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds129344.mlab.com:29344/heroku_894w2hzh';
+    connectionString += '@ds259070.mlab.com:59070/heroku_qrc41fks';
 }
 
 var mongoose = require("mongoose");
